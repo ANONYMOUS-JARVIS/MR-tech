258466e2cdf013b846a361613dd8466e4eba8356
ANONYMOUS-JARVIS <muthurajpostbox@gmail.com>
2024-09-06 14:10:33 +0530

    file updated

--- core/views.py
+++ core/views.py
@@ -13,7 +13,6 @@ import os
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
 from django.contrib.staticfiles import finders
 from django.contrib import messages
 from django.core.mail import send_mail
